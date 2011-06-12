d55b74858ed32c7e24149c63cbb4626503c7e4d8
Caleb Cushing <xenoterracide@gmail.com>
2011-06-12 11:56:30 -0500

    Build results of ab8f4d1 (on master)

--- lib/Perl/PrereqScanner/Scanner/Catalyst.pm
+++ lib/Perl/PrereqScanner/Scanner/Catalyst.pm
@@ -3,7 +3,7 @@ use 5.006;
 use strict;
 use warnings;
 BEGIN {
-	# VERSION
+	our $VERSION = '0.1.0'; # VERSION
 }
 use Moose;
 with 'Perl::PrereqScanner::Scanner';
@@ -14,3 +14,29 @@ sub scan_for_prereqs {
 1;
 
 # ABSTRACT: Scan for Modules loaded by Catalyst
+
+__END__
+=pod
+
+=head1 NAME
+
+Perl::PrereqScanner::Scanner::Catalyst - Scan for Modules loaded by Catalyst
+
+=head1 VERSION
+
+version 0.1.0
+
+=head1 AUTHOR
+
+Caleb Cushing <xenoterracide@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2011 by Caleb Cushing.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+=cut
+
